d5cc18d6ed78a74b4463153c1950d9500603fab7
zhangyifeng <zhangyifeng@joysfintech.com>
2021-09-23 14:08:56 +0800

    将图形验证码加入redis中，引入redis中间件，未测试，已部署redis容器服务

--- xdclass-user-service/src/main/java/net/xdclass/controller/NotifyController.java
+++ xdclass-user-service/src/main/java/net/xdclass/controller/NotifyController.java
@@ -4,8 +4,10 @@ import com.google.code.kaptcha.Producer;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import net.xdclass.utils.CommonUtil;
 import net.xdclass.utils.JsonData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Api(tags = "通知模块")
@@ -26,10 +29,22 @@ public class NotifyController {
     @Autowired
     private Producer captchaProducer;
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    /**
+     * 临时使用10分钟有效，方便测试
+     */
+    private static final long CAPTCHA_CODE_EXPIRED = 60 * 1000 * 10;
+
     @ApiOperation("获取图形验证码")
     @GetMapping("/captcha")
     public void getCapt(HttpServletRequest request, HttpServletResponse response) {
         String text = captchaProducer.createText();
+        log.info("图形验证码：{}", text);
+
+        redisTemplate.opsForValue().set(getCapachaKey(request), text, CAPTCHA_CODE_EXPIRED, TimeUnit.SECONDS);
+
         BufferedImage bufferedImage = captchaProducer.createImage(text);
         ServletOutputStream outputStream = null;
         try {
@@ -40,7 +55,7 @@ public class NotifyController {
         } catch (IOException e) {
             log.error("获取图形验证码异常:{}", e);
         }
-        log.info("图形验证码：{}", text);
+
     }
 
     @GetMapping("/captcha/test")
@@ -48,5 +63,17 @@ public class NotifyController {
         String text = captchaProducer.createText();
         log.info("图形验证码：{}", text);
         return JsonData.buildSuccess(text);
+    }
+
+    private String getCapachaKey(HttpServletRequest request) {
+        String ipAddr = CommonUtil.getIpAddr(request);
+        String userAgent = request.getHeader("User-Agent");
+        String key = "user-service:captcha:" + CommonUtil.MD5(ipAddr + userAgent);
+
+        log.info("ip={}", ipAddr);
+        log.info("userAgent={}", userAgent);
+        log.info("key={}", key);
+
+        return key;
     }
 }
