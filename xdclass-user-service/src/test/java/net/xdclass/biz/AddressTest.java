ab2e5aae568890536ebb22530061f36c525d5371
zhangyifeng <zhangyifeng@joysfintech.com>
2021-09-26 16:37:39 +0800

    minio镜像--9.3<完>

--- xdclass-user-service/src/test/java/net/xdclass/biz/AddressTest.java
+++ xdclass-user-service/src/test/java/net/xdclass/biz/AddressTest.java
@@ -12,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
- * 地址相关单元测试类
+ * 地址相关功能单元测试类
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = UserApplication.class)
